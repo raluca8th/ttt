b4438b0996b779d89aaf1f388fc96fbbe2ca795d
Raluca <raluca8th@gmail.com>
2016-07-31 17:30:26 -0500

    Add custom board size

--- board/board.go
+++ board/board.go
@@ -1,8 +1,16 @@
 package board
 
 type Board struct {
+  size int
 }
 
-func (b Board) Size() int{
-  return 9
+func (b Board) Size() int {
+  b.setDefaultSize()
+  return b.size
+}
+
+func (b *Board) setDefaultSize() {
+  if b.size == 0{
+    b.size = 9
+  }
 }
--- board/board_test.go
+++ board/board_test.go
@@ -9,3 +9,11 @@ func TestDefaultSize(t *testing.T) {
     t.Error("Expected board size to be 9, but it was", boardSize)
   }
 }
+
+func TestCustomSize(t *testing.T) {
+  board := Board{size: 16}
+  boardSize := board.Size()
+  if boardSize != 16 {
+    t.Error("Expected board size to be 16, but it was", boardSize)
+  }
+}
