6eb5c1592230cab4a2951238b7cececb24309d19
Raluca <raluca8th@gmail.com>
2016-07-31 19:32:47 -0500

    Add board surface

--- board/board.go
+++ board/board.go
@@ -3,16 +3,31 @@ package board
 type Board struct {
   size int
   markers [2]string
+  surface []string
+}
+
+func NewBoard(params Params) *Board {
+  board := &Board{size: params.size, markers: params.markers}
+  board.setDefaultSize()
+  board.setDefaultMarkers()
+  board.setSurface()
+  return board
 }
 
 func (b Board) Size() int {
-  b.setDefaultSize()
   return b.size
 }
 
 func (b Board) Markers() [2]string {
-  b.setDefaultMarkers()
   return b.markers
+}
+
+func (b Board) Surface() []string {
+  return b.surface
+}
+
+func (b *Board) setSurface() {
+  b.surface = make([]string, b.Size())
 }
 
 func (b *Board) setDefaultSize() {
--- board/board_test.go
+++ board/board_test.go
@@ -2,8 +2,13 @@ package board
 
 import "testing"
 
+type Params struct {
+  size int
+  markers [2]string
+}
+
 func TestDefaultSize(t *testing.T) {
-  board := new(Board)
+  board := NewBoard(Params{})
   boardSize := board.Size()
   if boardSize != 9 {
     t.Error("Expected board size to be 9, but it was", boardSize)
@@ -11,7 +16,7 @@ func TestDefaultSize(t *testing.T) {
 }
 
 func TestSize(t *testing.T) {
-  board := Board{size: 16}
+  board := NewBoard(Params{size: 16})
   boardSize := board.Size()
   if boardSize != 16 {
     t.Error("Expected board size to be 16, but it was", boardSize)
@@ -19,7 +24,7 @@ func TestSize(t *testing.T) {
 }
 
 func TestMarkers(t *testing.T) {
-  board := Board{markers: [2]string{"A", "B"}}
+  board := NewBoard(Params{markers: [2]string{"A", "B"}})
   boardMarkers := board.Markers()
   if boardMarkers != [2]string{"A", "B"} {
     t.Error("Expected markers to be 'A' and 'B', but they were", boardMarkers)
@@ -27,9 +32,21 @@ func TestMarkers(t *testing.T) {
 }
 
 func TestDefaultMarkers(t *testing.T) {
-  board := new(Board)
+  board := NewBoard(Params{})
   boardMarkers := board.Markers()
   if boardMarkers != [2]string{"X", "Y"} {
     t.Error("Expected markers to be 'X' and 'Y', but they were", boardMarkers)
+  }
+}
+
+func TestNewBoardSurface(t *testing.T) {
+  board := NewBoard(Params{})
+  boardSurface := board.Surface()
+  if surfaceLenght := len(boardSurface); surfaceLenght != 9 {
+    t.Error("Expected board surface to have lenght of 9, but it was", surfaceLenght)
+  }
+
+  if spot := boardSurface[3]; spot != "" {
+    t.Error("Expected spot to be empty but it was", spot)
   }
 }
