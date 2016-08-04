24c492be4827326a86cd5b6ef524ffe911e0b8f9
Raluca <raluca8th@gmail.com>
2016-08-04 09:50:47 -0500

    Add setup welcome message and player name selection

---
+++ setup/setup.go
@@ -0,0 +1,28 @@
+package setup
+
+type setupUI interface{
+  Read() string
+  Print(s string)
+}
+
+type Setup struct{
+  Ui setupUI
+}
+
+func (s *Setup) Welcome(){
+  s.print("Welcome to GO TicTacToe")
+}
+
+func (s *Setup) GetPlayerName() string{
+  s.print("Please enter player name")
+  input := s.read()
+  return input
+}
+
+func (s *Setup) print(st string){
+  s.Ui.Print(st)
+}
+
+func (s *Setup) read() string{
+  return s.Ui.Read()
+}
---
+++ setup/setup_test.go
@@ -0,0 +1,67 @@
+package setup
+
+import (
+  "testing"
+  "bytes"
+)
+
+func TestWelcome(t *testing.T){
+  testUI := TestUI{Input: new(testSTDIN), Output: new(testSTDOUT)}
+  setUp := Setup{Ui: testUI}
+  expectedMessage := "Welcome to GO TicTacToe"
+  setUp.Welcome()
+
+  if welcomeMessage := testUI.CheckOutput(); welcomeMessage != expectedMessage {
+    t.Error("Expected welcome message, but got", welcomeMessage)
+  }
+}
+
+func TestNameSelection(t *testing.T){
+  testUI := TestUI{Input: new(testSTDIN), Output: new(testSTDOUT)}
+  testUI.Populate("Anda")
+  setUp := Setup{Ui: testUI}
+  expectedMessage := "Anda"
+
+  if playerName := setUp.GetPlayerName(); playerName != expectedMessage {
+    t.Error("Expected 'please enter username', but got", playerName)
+  }
+}
+
+type testSTDIN struct {
+  buffer bytes.Buffer
+}
+
+func (r *testSTDIN) read() string{
+  input, _ := r.buffer.ReadString(' ')
+  return input
+}
+
+type testSTDOUT struct {
+  buffer bytes.Buffer
+}
+
+func (p *testSTDOUT) print(s string){
+  p.buffer.WriteString(s)
+}
+
+type TestUI struct{
+  Input *testSTDIN
+  Output *testSTDOUT
+}
+
+func (ui TestUI) Read() string{
+  return ui.Input.read()
+}
+
+func (ui TestUI) Print(s string){
+  ui.Output.print(s)
+}
+
+func (ui *TestUI) CheckOutput() string{
+  input, _ := ui.Output.buffer.ReadString('#')
+  return input
+}
+
+func (ui *TestUI) Populate(s string){
+   ui.Input.buffer.WriteString(s)
+}
