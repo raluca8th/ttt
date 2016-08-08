44f6586cb0f35ffd5baa29cf6127f75d3daa0847
Raluca <raluca8th@gmail.com>
2016-08-08 14:17:41 -0500

    Add human and computer games

--- main_test.go
+++ main_test.go
@@ -13,11 +13,37 @@ func TestHumanVsHumanGame(t *testing.T){
   g := &gameInitializer{ui: ui}
   g.playGame()
   finalBoardState := g.game.Board().Surface()
-  t.Log(finalBoardState)
   expectedBoardState := []string{"X", "X", "X", "Y", "Y", "", "", "", ""}
 
   if !reflect.DeepEqual(finalBoardState, expectedBoardState) {
     t.Error("Expected X to win, but final board state was", finalBoardState)
+  }
+}
+
+func TestHumanVsComputer(t *testing.T){
+  stdin := new(testSTDIN)
+  stdin.populateBuffer("1 2 Anda X Wallee W 1 4 2 3 5 6")
+  stdout := new(testSTDOUT)
+  ui := testUI{input: stdin, output: stdout}
+  g := &gameInitializer{ui: ui}
+  g.playGame()
+  humanMarker := "X"
+
+  if g.game.Board().WinningMarker() == humanMarker {
+    t.Error("Expected computer to win or tie, but winning marker was", humanMarker)
+  }
+}
+
+func TestComputerVsComputer(t *testing.T){
+  stdin := new(testSTDIN)
+  stdin.populateBuffer("1 3 Eve E Wallee W")
+  stdout := new(testSTDOUT)
+  ui := testUI{input: stdin, output: stdout}
+  g := &gameInitializer{ui: ui}
+  g.playGame()
+
+  if g.game.Board().IsTiedBoard() != true {
+    t.Error("Expected computer vs. computer to always end in a tie")
   }
 }
 
