0b970708dfe513ccb04fb9b7a7de5407a6b611ce
Raluca <raluca8th@gmail.com>
2016-08-08 10:02:27 -0500

    Add Human vs. Human game

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "fmt"
+import (
+  "github.com/raluca8th/ttt/setup"
+  "github.com/raluca8th/ttt/cli"
+  "github.com/raluca8th/ttt/game"
+  "github.com/raluca8th/ttt/ui"
+)
 
-func main() {
-  fmt.Println(welcome())
+func main(){
+  ui := new(cli.CLI)
+  g := gameInitializer{ui: ui}
+  g.playGame()
 }
 
-func welcome() string{
-  return "Welcome to my TTT"
+func size(selection string) int{
+  if selection == "1"{
+    return 9
+  } else {
+    return 16
+  }
+}
+
+type gameInitializer struct{
+  setup *setup.Setup
+  game *game.Game
+  ui ui.UI
+}
+
+func (g *gameInitializer) playGame(){
+  g.setup = &setup.Setup{Ui: g.ui}
+  g.setup.Welcome()
+  sizeSelection := g.setup.GetGameSize()
+  size := size(sizeSelection)
+  gameType := g.setup.GetGameType()
+  players := g.setup.GeneratePlayers(gameType)
+  g.game = game.NewGame(players, size, g.ui)
+  g.game.PlayGame()
 }
--- main_test.go
+++ main_test.go
@@ -1,16 +1,58 @@
 package main
 
 import "testing"
+import "reflect"
+import "bytes"
+import "strings"
 
-func ExampleWelcome() {
-  main()
-  // Output: Welcome to my TTT
+func TestHumanVsHumanGame(t *testing.T){
+  stdin := new(testSTDIN)
+  stdin.populateBuffer("1 1 Anda X Eli Y 1 4 2 3")
+  stdout := new(testSTDOUT)
+  ui := testUI{input: stdin, output: stdout}
+  g := &gameInitializer{ui: ui}
+  g.playGame()
+  finalBoardState := g.game.Board().Surface()
+  t.Log(finalBoardState)
+  expectedBoardState := []string{"X", "X", "X", "Y", "Y", "", "", "", ""}
+
+  if !reflect.DeepEqual(finalBoardState, expectedBoardState) {
+    t.Error("Expected X to win, but final board state was", finalBoardState)
+  }
 }
 
-func TestHello(t *testing.T) {
-  expected := "Welcome to my TTT"
-  returned := welcome()
-  if returned != expected {
-    t.Fatalf("Expected %s, got %s", expected, returned)
+type testSTDIN struct {
+  buffer bytes.Buffer
+}
+
+func (r *testSTDIN) Read() string{
+  input, _ := r.buffer.ReadString(' ')
+  return strings.TrimSpace(input)
+}
+
+func (p *testSTDIN) populateBuffer(s string){
+  p.buffer.WriteString(s)
+}
+
+type testSTDOUT struct {
+  buffer bytes.Buffer
+}
+
+func (p testSTDOUT) Print(s string) {
+  p.buffer.WriteString(s)
+}
+
+type testUI struct{
+  input *testSTDIN
+  output *testSTDOUT
+}
+
+func (ui testUI) Read() string{
+  return ui.input.Read()
+}
+
+func (ui testUI) Print(out ...string) {
+  for _, s := range out {
+    ui.output.Print(s)
   }
 }
